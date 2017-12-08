fd4ec63789739af04a5db13f32a2b27a9bdbbf0a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-12-07 18:29:14 -0600

    Further work on cleanly mapping errors in promises/futures
    Ref https://github.com/alexcrichton/futures-rs/issues/671

--- src/main.rs
+++ src/main.rs
@@ -95,76 +95,23 @@ fn forward(bind_ip: &str, local_port: i32, remote_host: &str, remote_port: i32) 
     //we have either been provided an IP address or a host name
     //instead of trying to check its format, just trying creating a SocketAddr from it
     let parse_result = format!("{}:{}", remote_host, remote_port).parse::<std::net::SocketAddr>();
-    let server = future::result(parse_result)
+
+    //ultimately trying to resolve to a FutureSomething<SocketAddr, String>
+    let addr = future::result(format!("{}:{}", remote_host, remote_port).parse::<std::net::SocketAddr>())
+        .map(|result| future::result(Ok(result))) //I'm pretty sure this is wrong. We only want a single level.
         .or_else(|_| {
-            //it's a hostname; we're going to need to resolve it
-            //create an async dns resolver
-            let resolver = DnsResolver::system_config(&handle).unwrap();
-
-            resolver.resolve(&format!("{}:{}", remote_host, remote_port))
-                .map(move |resolved| {
-                    resolved.pick_one()
-                        .expect(&format!("No valid IP addresses for target {}", remote_host))
+            //it's a hostname; we're going to need to resolve it.
+            future::result(DnsResolver::system_config(&handle))
+                .map(|resolver| {
+                    resolver.resolve(&format!("{}:{}", remote_host, remote_port))
+                        .map(move |resolved| {
+                            resolved.pick_one()
+                                .expect(&format!("No valid IP addresses for target {}", remote_host))
+                        })
                 })
-                .map_err(|err| println!("{:?}", err))
-        })
-        .and_then(|remote_addr| {
-            println!("Resolved {}:{} to {}",
-                     remote_host,
-                     remote_port,
-                     remote_addr);
-
-            let remote_addr = remote_addr.clone();
-            let handle = handle.clone();
-            listener.incoming()
-                .for_each(move |(client, client_addr)| {
-                    println!("New connection from {}", client_addr);
-
-                    //establish connection to upstream for each incoming client connection
-                    let handle = handle.clone();
-                    TcpStream::connect(&remote_addr, &handle).and_then(move |remote| {
-                        let (client_recv, client_send) = client.split();
-                        let (remote_recv, remote_send) = remote.split();
-
-                        let remote_bytes_copied = io::copy(remote_recv, client_send);
-                        let client_bytes_copied = io::copy(client_recv, remote_send);
-
-                        fn error_handler<T, V>(err: T, client_addr: V)
-                            where T: std::fmt::Debug,
-                                  V: std::fmt::Display
-                        {
-                            println!("Error writing from upstream server to remote client {}!",
-                                     client_addr);
-                            println!("{:?}", err);
-                            ()
-                        };
-
-                        let client_addr_clone = client_addr.clone();
-                        let async1 = remote_bytes_copied.map(move |(count, _, _)| {
-                                debug(format!("Transferred {} bytes from upstream server to \
-                                               remote client {}",
-                                              count,
-                                              client_addr_clone))
-                            })
-                            .map_err(move |err| error_handler(err, client_addr_clone));
-
-                        let client_addr_clone = client_addr;
-                        let async2 = client_bytes_copied.map(move |(count, _, _)| {
-                                debug(format!("Transferred {} bytes from remote client {} to \
-                                               upstream server",
-                                              count,
-                                              client_addr_clone))
-                            })
-                            .map_err(move |err| error_handler(err, client_addr_clone));
-
-                        handle.spawn(async1);
-                        handle.spawn(async2);
-
-                        Ok(())
-                    })
-                })
-                .map_err(|err| println!("{:?}", err))
         });
 
-    core.run(server).unwrap();
+    //we want core.run(xxx) to resolve to a single-level Result<_, _>, and not
+    //Result<_, Result<_>>
+    core.run(addr).unwrap();
 }
